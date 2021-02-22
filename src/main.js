f62bf063bd49cae0cb28ffc1b585be19329c6f53
a-hirata <hirataakira6@gmail.com>
2021-02-22 16:00:00 +0900

    google

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import SVG from "./components/SVG.vue";
-// import VueGtag from "vue-gtag-next";
+import VueGtag from "vue-gtag-next";
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.use(VueGtag, {
+  property: {
+    id: "G-2T1J8R59SX"
+  }
+});
+
 app.component("SVG", SVG);
 
 app.mount("#app");
